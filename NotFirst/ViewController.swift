91d255d1f0721243a91b25e6a4814478f648e749
tejasdev749 <tejasdev749@gmail.com>
2018-09-22 12:25:28 +0530

    Not First iOS Dev Project

--- NotFirst/ViewController.swift
+++ NotFirst/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let label = UILabel()
+    
+    
+    init() {
+    
+        super.init(nibName: nil, bundle: nil)
+        
+        label.text = "Without Storyboard"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.red
+        label.frame = CGRect(origin: CGPoint(x: 100, y: 500), size: CGSize(width: self.view.frame.width, height: 30))
+        
+        self.view.addSubview(label)
+        
     }
 
     override func didReceiveMemoryWarning() {
